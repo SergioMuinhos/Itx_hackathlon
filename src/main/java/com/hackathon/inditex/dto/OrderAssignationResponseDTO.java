d4af81d422094ee312ea920a6191bfd745b9dc98
Muiños <sergiomuinosgarcia@gmail.com>
2025-03-27 09:36:46 +0100

    Task4_OrdersAssigment:Done

--- src/main/java/com/hackathon/inditex/dto/OrderAssignationResponseDTO.java
+++ src/main/java/com/hackathon/inditex/dto/OrderAssignationResponseDTO.java
@@ -1,10 +1,12 @@
 package com.hackathon.inditex.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class OrderAssignationResponseDTO {
+    @JsonProperty("processed-orders")
     private List<ProcessedOrderDTO> processedOrders;
 }
